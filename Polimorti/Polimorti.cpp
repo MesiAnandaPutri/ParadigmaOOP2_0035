be00c9b82e77023655324224cd11d9d781e8722d
MesiAnandaPutri <mesianandaputri24@gmail.com>
2024-05-28 17:52:12 +0700

    menjalankan program utama

--- Polimorti/Polimorti.cpp
+++ Polimorti/Polimorti.cpp
@@ -22,3 +22,15 @@ public:
 		cout << "Pesan dari Lia" << endl;
 	}
 };
+
+int main() {
+	seseorang* obyek;
+	joko a;
+	lia b;
+
+	obyek = &a;
+	obyek->pesan();
+	obyek = &b;
+	obyek->pesan();
+	//a.seseorang::pesan();
+}
