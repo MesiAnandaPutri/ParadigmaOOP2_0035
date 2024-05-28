cdeb83319acd83c78dd0eb20e6da4ca3ba47de70
MesiAnandaPutri <mesianandaputri24@gmail.com>
2024-05-28 18:58:10 +0700

    menampilkan nilai

--- abstraksi/abstraksi.cpp
+++ abstraksi/abstraksi.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-class abstraksiklas {
+class Abstraksiklas {
 private: string x, y;
 
 public:
@@ -11,3 +11,9 @@ public:
 		x = a;
 		y = b;
 	}
+	// menampilkan nilai
+	void display() {
+		cout << "x =" << x << endl;
+		cout << "y =" << y << endl;
+	}
+};
