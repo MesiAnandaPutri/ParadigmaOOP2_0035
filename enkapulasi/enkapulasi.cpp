d60a880adac54e64148f5b319d2afed3842b6f6e
MesiAnandaPutri <mesianandaputri24@gmail.com>
2024-05-28 18:37:36 +0700

    menjalankan program utama dan menampilkan remote lampu

--- enkapulasi/enkapulasi.cpp
+++ enkapulasi/enkapulasi.cpp
@@ -12,3 +12,19 @@ public:
 		return saklarNo[i];
 	}
 };
+
+int main() {
+	remoteLampu lampuRumah;
+
+	lampuRumah.setsaklarNo(0, "Lampu Teras Rumah");
+	lampuRumah.setsaklarNo(1, "Lampu Teras tamu");
+	lampuRumah.setsaklarNo(2, "Lampu Teras tidur");
+	lampuRumah.setsaklarNo(3, "Lampu Dapur");
+
+	cout << lampuRumah.getsaklarNo(0) << endl;
+	cout << lampuRumah.getsaklarNo(1) << endl;
+	cout << lampuRumah.getsaklarNo(2) << endl;
+	cout << lampuRumah.getsaklarNo(3) << endl;
+
+	return 0;
+}
